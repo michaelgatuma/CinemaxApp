100f8c30135b2faec79c26893b1fdfd3cf185bd0
Eleandro Duzentos <eleandro@inbox.ru>
2019-07-01 17:33:27 +0100

    Added GraphQL client

---
+++ lib/services/graphql.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+final HttpLink httpLink = HttpLink(
+  uri: 'https://api.cinemax.co.ao7graphql'
+);
+
+ValueNotifier<GraphQLClient> client = ValueNotifier(
+  GraphQLClient(
+    cache: InMemoryCache(),
+    link: httpLink as Link
+  )
+);
