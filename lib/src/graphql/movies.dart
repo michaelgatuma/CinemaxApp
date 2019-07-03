80bc2b63510f08d4442ecc9bdfca402fab048781
Eleandro Duzentos <eleandro@inbox.ru>
2019-07-03 12:04:57 +0100

    Added upcoming movies query

--- lib/src/graphql/movies.dart
+++ lib/src/graphql/movies.dart
@@ -1,4 +1,4 @@
-const MOVIES_QUERY = """
+const MOVIES_FEATURING_TODAY_QUERY = """
   query FEATURING_TODAY_MOVIES_QUERY(\$sessionCategory: String) {
     moviesFeaturingToday: moviesFeaturingToday(
       sessionCategory: \$sessionCategory
@@ -9,10 +9,37 @@ const MOVIES_QUERY = """
       trailerId
       hasSessions
       genres {
+        id
         name
       }
       picture {
+        id
         thumb(size: COVER) {
+          id
+          url
+        }
+      }
+    }
+  }
+""";
+
+const UPCOMING_MOVIES_QUERY = """
+  query UPCOMING_MOVIES_QUERY {
+    upcomingMovies {
+      id
+      name
+      slug
+      debut
+      trailerId
+      hasSessions
+      genres {
+        id
+        name
+      }
+      picture {
+        id
+        thumb(size: COVER) {
+          id
           url
         }
       }
