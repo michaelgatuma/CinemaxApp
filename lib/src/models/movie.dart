6c32a6413467d60a365509111ef93cf41ed910e8
Eleandro Duzentos <eleandro@inbox.ru>
2019-07-03 11:10:09 +0100

    Updated movie model name

---
+++ lib/src/models/movie.dart
@@ -0,0 +1,13 @@
+class MovieModel
+{
+  var id;
+  var name;
+  var slug;
+
+  var trailerId;
+  var genres;
+
+  var coverUrl;
+
+  MovieModel({ this.id, this.name, this.slug, this.trailerId, this.coverUrl, this.genres });
+}
