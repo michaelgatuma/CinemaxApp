0f56df66f8320026af9bddf7f846860d96380883
Eleandro Duzentos <eleandro@inbox.ru>
2019-07-03 16:28:04 +0100

    Added single movie query

--- lib/src/graphql/movies.dart
+++ lib/src/graphql/movies.dart
@@ -1,3 +1,35 @@
+const MOVIE_QUERY = """
+  query MOVIE_QUERY(\$id: ID!) {
+    movie(id: \$id) {
+      id
+      name
+      slug
+      debut
+      hasSessions
+      releaseYear
+      duration
+      ageLimit
+      summary
+      trailerId
+      stars
+      argumenters
+      directors
+      picture {
+        id
+        altText
+        thumb(size: COVER) {
+          id
+          url
+        }
+      }
+      genres {
+        id
+        name
+      }
+    }
+  }
+""";
+
 const MOVIES_FEATURING_TODAY_QUERY = """
   query FEATURING_TODAY_MOVIES_QUERY(\$sessionCategory: String) {
     moviesFeaturingToday: moviesFeaturingToday(
