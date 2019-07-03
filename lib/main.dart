10c42d7797536f01a2fdf679f055eb31c252d4e5
Eleandro Duzentos <eleandro@inbox.ru>
2019-07-03 11:06:48 +0100

    Moved CinemaxApp class to its single file

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
-
-import './routes.dart';
-
-import './graphql/client.dart';
+import 'package:cinemax_app/src/app.dart';
 
 void main() => runApp(CinemaxApp());
 
-class CinemaxApp extends StatelessWidget {
-  CinemaxApp({ Key key }): super(key: key);
-
-  final _appTitle = "Cinemax";
-
-  @override
-  Widget build(BuildContext context) {
-    return GraphQLProvider(
-      client: client,
-      child: MaterialApp(
-        title: _appTitle,
-        theme: ThemeData(
-          accentColor: Colors.orange[900],
-          primaryColor: Color(0xffff9800),
-          brightness: Brightness.dark
-        ),
-        initialRoute: '/',
-        routes: routes,
-        debugShowCheckedModeBanner: false,
-      )
-    );
-  }
-}
