24168529eb68739ef904a2070bde5c4b22e0e352
Eleandro Duzentos <eleandro@inbox.ru>
2019-07-03 15:17:59 +0100

    Added longDebut and month names list

--- lib/src/models/movie.dart
+++ lib/src/models/movie.dart
@@ -10,7 +10,37 @@ class MovieModel
 
   var coverUrl;
 
+  final List<String> months = [
+    'Jan',
+    'Fev',
+    'Abr',
+    'Mar',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Set',
+    'Out',
+    'Nov',
+    'Dez'
+  ];
+
   MovieModel({ this.id, this.name, this.slug, this.debut, this.trailerId, this.coverUrl, this.genres });
 
   String get genresList => genres.map((genre) => genre['name']).join(', ');
+
+  String get longDebut => _getLongDebut(debut);
+
+  String _getLongDebut(debut) {
+    if (debut != null) {
+      final date = DateTime.parse(debut);
+
+      final day = date.day;
+      final month = months[date.month - 1];
+      final year = date.year;
+
+      return '$day $month, $year';
+    }
+
+    return '';
+  }
 }
