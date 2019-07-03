f2a0f3bd20928f4dd89906a8a4a8655ef0869ef6
Eleandro Duzentos <eleandro@inbox.ru>
2019-07-03 16:28:49 +0100

    Added page movie.dart

---
+++ lib/src/pages/movie.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MoviePage extends StatefulWidget {
+  MoviePage({Key key}) : super(key: key);
+
+  _MoviePageState createState() => _MoviePageState();
+}
+
+class _MoviePageState extends State<MoviePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(""),
+    );
+  }
+}
