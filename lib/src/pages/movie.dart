80e3450a209eec49079cacb20c83089166876675
Eleandro Duzentos <eleandro@inbox.ru>
2019-07-03 17:37:14 +0100

    Added view code to MoviePage

--- lib/src/pages/movie.dart
+++ lib/src/pages/movie.dart
@@ -1,16 +1,55 @@
+import 'dart:ui';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cinemax_app/src/blocs/movie_bloc.dart';
+import 'package:cinemax_app/src/models/movie.dart';
 import 'package:flutter/material.dart';
 
-class MoviePage extends StatefulWidget {
-  MoviePage({Key key}) : super(key: key);
+class MoviePage extends StatelessWidget {
+  final int movieId;
 
-  _MoviePageState createState() => _MoviePageState();
-}
+  MoviePage({ this.movieId });
 
-class _MoviePageState extends State<MoviePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(""),
+    movieBloc.fetchMovie(movieId);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Filme')
+      ),
+      body: StreamBuilder(
+        stream: movieBloc.movie,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            final _movie = snapshot.data as MovieModel;
+
+            return Container(
+              child: Column(
+                children: <Widget>[
+                  Stack(children: <Widget>[
+                    Expanded(
+                      child: CachedNetworkImage(
+                        imageUrl: _movie.coverUrl,
+                        fit: BoxFit.fill,
+                      )
+                    ),
+                    BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                      child: Container(
+                        color: Colors.black.withOpacity(0.3)
+                      ),
+                    ),
+                  ]),
+                  Text(_movie.name)
+                ]
+              )
+            );
+          }
+
+          return Center(child: CircularProgressIndicator());
+        },
+      )
     );
   }
 }
