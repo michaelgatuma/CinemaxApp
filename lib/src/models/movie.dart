d4ba4d4781e5834ac0d81da5d57b6193b5e57f53
Eleandro Duzentos <eleandro@inbox.ru>
2019-07-03 12:28:22 +0100

    Added genresList string

--- lib/src/models/movie.dart
+++ lib/src/models/movie.dart
@@ -10,4 +10,6 @@ class MovieModel
   var coverUrl;
 
   MovieModel({ this.id, this.name, this.slug, this.trailerId, this.coverUrl, this.genres });
+
+  String get genresList => genres.map((genre) => genre['name']).join(', ');
 }
