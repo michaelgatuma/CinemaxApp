f23fc9b70facef34dd3984f56adbee6cecc82be5
Eleandro Duzentos <eleandro@inbox.ru>
2019-07-03 11:10:27 +0100

    Added movies_api_provider.dart

---
+++ lib/src/services/movies_api_provider.dart
@@ -0,0 +1,42 @@
+import 'package:cinemax_app/graphql/movies.dart';
+import 'package:cinemax_app/src/models/factories/movie_factory.dart';
+import 'package:cinemax_app/src/models/movie.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+class MoviesApiProvider {
+  GraphQLClient _client;
+
+  MoviesApiProvider() {
+    final HttpLink httpLink = HttpLink(
+      uri: 'https://api.cinemax.co.ao/graphql'
+    );
+
+    _client = GraphQLClient(
+      cache: InMemoryCache(),
+      link: httpLink as Link
+    );
+  }
+
+  Future<List<MovieModel>> fetchMoviesFeaturingToday(String movieSessionCategory) async {
+    final res = await _client.query(
+      QueryOptions(
+        document: MOVIES_QUERY,
+        variables: {
+          'sessionCategory': movieSessionCategory
+        }
+      )
+    );
+
+    List<MovieModel> _movieList = List<MovieModel>();
+
+    final jsonMovies = res.data['moviesFeaturingToday'];
+
+    for (const _movie in jsonMovies) {
+      final _movieModel = MovieFactory.makeFromJson(_movie);
+
+      _movieList.add((_movieModel));
+    }
+
+    return _movieList;
+  }
+}
