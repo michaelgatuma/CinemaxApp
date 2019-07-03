4ba9418a971993173c2590a887bd89e93ff857ad
Eleandro Duzentos <eleandro@inbox.ru>
2019-07-03 11:47:57 +0100

    Replaced Query Widget in favour of StreamBuilder

--- lib/src/ui/movies.dart
+++ lib/src/ui/movies.dart
@@ -1,63 +1,46 @@
-import 'package:cinemax_app/graphql/movies.dart';
+import 'package:cinemax_app/src/blocs/movie_block.dart';
+import 'package:cinemax_app/src/models/movie.dart';
+import 'package:cinemax_app/src/ui/movie.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
 
-class MovieList extends StatefulWidget {
-  final sessionCategory;
+class MovieList extends StatelessWidget {
+  final String movieSessionCategory;
 
-  MovieList({ this.sessionCategory });
+  MovieList({ this.movieSessionCategory });
 
-  @override
-  _MovieListState createState() => _MovieListState();
-}
-
-class _MovieListState extends State<MovieList> {
   @override
   Widget build(BuildContext context) {
-    final sessionCategory = widget.sessionCategory;
+    moviesBlock.fetchMoviesfeaturingToday(movieSessionCategory: movieSessionCategory);
 
-    return Query(
-      builder: (QueryResult result, { VoidCallback refetch }) {
-        if (result.errors != null) {
-          return Center(child: Text(result.errors.toString()));
+    return StreamBuilder(
+      stream: moviesBlock.moviesfeaturingToday,
+      builder: (BuildContext context, AsyncSnapshot<List<MovieModel>> _moviesList) {
+        if (_moviesList.hasData) {
+          if (_moviesList.data.length == 0) {
+            return Center(child: Text('Não há filmes em exibição nesta sessão'));
+          }
+
+          return StaggeredGridView.countBuilder(
+            crossAxisCount: 2,
+            itemCount: _moviesList.data.length,
+            physics: AlwaysScrollableScrollPhysics(),
+            itemBuilder: (BuildContext context, int index) {
+              final _movie = _moviesList.data[index];
+
+              return Movie(movie: _movie);
+            },
+            staggeredTileBuilder: (int index) {
+              return StaggeredTile.fit(1);
+            },
+            mainAxisSpacing: 15.0,
+            crossAxisSpacing: 15.0,
+            padding: EdgeInsets.all(15.0),
+          );
         }
 
-        if (result.loading) {
-          return Center(child: CircularProgressIndicator());
-        }
-
-        List _movies = result.data['moviesFeaturingToday'];
-
-        if (_movies.length == 0) {
-          return Center(child: Text('Não há filmes em exibição nesta sessão'));
-        }
-
-        return StaggeredGridView.countBuilder(
-          crossAxisCount: 2,
-          itemCount: _movies.length,
-          physics: AlwaysScrollableScrollPhysics(),
-          itemBuilder: (BuildContext context, int index) {
-            /* final _movie = _movies[index];
-
-            final _movieModel = MovieFactory.makeFromJson(_movie);
-
-            return Movie(movie: _movieModel); */
-          },
-          staggeredTileBuilder: (int index) {
-            return StaggeredTile.fit(1);
-          },
-          mainAxisSpacing: 15.0,
-          crossAxisSpacing: 15.0,
-          padding: EdgeInsets.all(15.0),
-        );
+        return Center(child: CircularProgressIndicator());
       },
-      options: QueryOptions(
-        document: MOVIES_QUERY,
-        variables: {
-          'sessionCategory': sessionCategory
-        }
-      )
     );
   }
 }
