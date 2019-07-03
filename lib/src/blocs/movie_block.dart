d7fcc08fb0cb4fa1bd5826852305ce558b69c6a0
Eleandro Duzentos <eleandro@inbox.ru>
2019-07-03 12:27:57 +0100

    Added _upcomingMoviesFetcher

--- lib/src/blocs/movie_block.dart
+++ lib/src/blocs/movie_block.dart
@@ -5,20 +5,30 @@ import 'package:rxdart/rxdart.dart';
 class MoviesBloc
 {
   final _repository = MoviesRepository();
-  final _moviesFetcher = PublishSubject<List<MovieModel>>();
 
-  Observable<List<MovieModel>> get moviesfeaturingToday => _moviesFetcher.stream;
+  final _moviesFeaturingTodayFetcher = PublishSubject<List<MovieModel>>();
+  final _upcomingMoviesFetcher       = PublishSubject<List<MovieModel>>();
+
+  Observable<List<MovieModel>> get moviesfeaturingToday => _moviesFeaturingTodayFetcher.stream;
+  Observable<List<MovieModel>> get upcomingMovies       => _upcomingMoviesFetcher.stream;
 
   fetchMoviesfeaturingToday({ String movieSessionCategory }) async {
     List<MovieModel> _moviesList = await _repository.fetchMoviesFeaturingToday(
       movieSessionCategory: movieSessionCategory
     );
 
-    _moviesFetcher.sink.add(_moviesList);
+    _moviesFeaturingTodayFetcher.sink.add(_moviesList);
+  }
+
+  fetchUpcomingMovies() async {
+    List<MovieModel> _moviesList = await _repository.fetchUpcomingMovies();
+
+    _upcomingMoviesFetcher.sink.add(_moviesList);
   }
 
   dispose() {
-    _moviesFetcher.close();
+    _moviesFeaturingTodayFetcher.close();
+    _upcomingMoviesFetcher.close();
   }
 }
 
